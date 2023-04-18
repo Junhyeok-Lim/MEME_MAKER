957680cbc8f734c100fd5edbaf65df31d544d6d4
Junhyeok-Lim <junh0301@naver.com>
2023-04-18 16:43:47 +0900

    Filling Mode

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const modeBtn = document.getElementById("mode-btn");
 const colorOptions = Array.from(document.getElementsByClassName("color-option"));
 const color = document.getElementById("color");
 const lineWidth = document.getElementById("line-width");
@@ -6,9 +7,9 @@ const ctx = canvas.getContext("2d");
 canvas.width=800;
 canvas.height=800;
 ctx.lineWidth = lineWidth.value;
-
-
 let isPainting = false;
+let isFilling = false;
+
 function onMove(event){
     if(isPainting){
         ctx.lineTo(event.offsetX, event.offsetY);
@@ -48,8 +49,27 @@ function onColorChange(event){
 }
 
 function onColorClick(event){
-    ctx.strokeStyle = event.target.dataset.color;
-    ctx.fillStyle = event.target.dataset.color;
+    const colorValue = event.target.dataset.color
+    ctx.strokeStyle = colorValue;
+    ctx.fillStyle = colorValue;
+    color.value = colorValue;
+}
+
+function onModeClick(){
+    if(isFilling){
+        isFilling = false;
+        modeBtn.innerText = "Fill";
+    }
+    else{
+        isFilling = true;
+        modeBtn.innerText = "Draw";
+    }
+}
+
+function onCanvasClick(){
+    if(isFilling){
+        ctx.fillRect(0,0,800,800);
+    }
 }
 
 canvas.addEventListener("mousemove", onMove);
@@ -58,8 +78,11 @@ canvas.addEventListener("mousedown", onMouseDown);
 //canvas.addEventListener("mouseleave", onMouseUp); // 마우스가 창 밖으로 나갔을 때는 MouseUp으로 감지
 canvas.addEventListener("mouseup", cancelPainting);
 canvas.addEventListener("mouseleave", cancelPainting);
+canvas.addEventListener("click", onCanvasClick);
 
 lineWidth.addEventListener("change", onLineWidthChange);
 color.addEventListener("change", onColorChange);
 
 colorOptions.forEach(color => color.addEventListener("click", onColorClick)); // 각각의 컬러에 이벤트리스너 할당
+
+modeBtn.addEventListener("click", onModeClick);
