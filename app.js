fe5d03b106e9a27f59fc2f2d7dd5cf3e43f37ae5
Junhyeok-Lim <junh0301@naver.com>
2023-04-18 14:35:14 +0900

    making_house_in_js

---
+++ app.js
@@ -0,0 +1,34 @@
+const canvas = document.querySelector("canvas");
+const ctx = canvas.getContext("2d");
+canvas.width=800;
+canvas.height=800;
+
+// ctx.rect(50, 50, 100, 100);
+// ctx.rect(150, 150, 100, 100);
+// ctx.rect(250, 250, 100, 100);
+// ctx.stroke();
+// ctx.fill();
+
+// ctx.beginPath();
+// ctx.rect(350, 350, 100, 100);
+// ctx.rect(450, 450, 100, 100);
+// ctx.fillStyle="red";
+// setTimeout(() => {ctx.fill();}, 5000);
+
+// ctx.moveTo(50, 50);
+// ctx.lineTo(150, 50);
+// ctx.lineTo(150, 150);
+// ctx.lineTo(50, 150);
+// ctx.lineTo(50, 50);
+// ctx.stroke();
+// ctx.fill();
+
+ctx.fillRect(200, 200, 50, 200);
+ctx.fillRect(400, 200, 50, 200);
+ctx.lineWidth = 2;
+ctx.fillRect(300, 300, 50, 100);
+ctx.fillRect(200, 200, 200, 20);
+ctx.moveTo(200, 200);
+ctx.lineTo(325, 100);
+ctx.lineTo(325, 200)
+ctx.fill();
