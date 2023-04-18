698516e9189631857b14e3a49f33a8568e48565b
Junhyeok-Lim <junh0301@naver.com>
2023-04-18 16:32:49 +0900

    Paint Color

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+const colorOptions = Array.from(document.getElementsByClassName("color-option"));
+const color = document.getElementById("color");
 const lineWidth = document.getElementById("line-width");
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
@@ -39,6 +41,17 @@ function onLineWidthChange(event){
     ctx.lineWidth = event.target.value;
 }
 
+function onColorChange(event){
+    //console.log(event.target.value);
+    ctx.strokeStyle = event.target.value;
+    ctx.fillStyle = event.target.value;
+}
+
+function onColorClick(event){
+    ctx.strokeStyle = event.target.dataset.color;
+    ctx.fillStyle = event.target.dataset.color;
+}
+
 canvas.addEventListener("mousemove", onMove);
 canvas.addEventListener("mousedown", onMouseDown);
 //canvas.addEventListener("mouseup", onMouseUp);
@@ -47,3 +60,6 @@ canvas.addEventListener("mouseup", cancelPainting);
 canvas.addEventListener("mouseleave", cancelPainting);
 
 lineWidth.addEventListener("change", onLineWidthChange);
+color.addEventListener("change", onColorChange);
+
+colorOptions.forEach(color => color.addEventListener("click", onColorClick)); // 각각의 컬러에 이벤트리스너 할당
